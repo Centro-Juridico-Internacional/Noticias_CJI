85708dab875ceceaaf6b2043c392f439e37c1b51
Desarrollo1 <desarrollo1@centrojuridicointernacional.com>
2025-05-27 15:06:54 -0500

    active server mode in astro.config.mjs and instal vercel package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
